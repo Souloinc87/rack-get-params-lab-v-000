85611ade21656e647ed67b87e091f052d8a01661
Marcus  = Soulo <souloinc87@gmail.com>
2019-01-25 03:18:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif @@cart.empty? 
+    elsif @@cart.empty?
       resp.write "You cart is empty"
     else
       resp.write "Path Not Found"
